c42fe08a2e619e97afca758de8d2f8fa64cf36be
Manuel Albarrán <weap88@gmail.com>
2016-07-03 23:23:37 +0200

    Added some constraints

--- priv/repo/migrations/20160703164704_add_category_id_to_video.exs
+++ priv/repo/migrations/20160703164704_add_category_id_to_video.exs
@@ -4,6 +4,13 @@ defmodule Rumbl.Repo.Migrations.AddCategoryIdToVideo do
   def change do
     alter table(:videos) do
       add :category_id, references(:categories)
+        # The references function accepts the :on_delete option with one of the following:
+        # :nothing
+        # The default.
+        # :delete_all
+        # When the category is deleted, all videos in that category are deleted.
+        # :nilify_all
+        # When a category is deleted, the category_id of all associated videos is set to NULL.
     end
   end
 end
