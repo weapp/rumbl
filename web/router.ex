efd6f07e35054d74a844b62d0368fa9a67c6fc0b
Manuel Albarrán <weap88@gmail.com>
2016-07-03 16:55:18 +0200

    Added video scaffold

--- web/router.ex
+++ web/router.ex
@@ -31,6 +31,12 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
   end
 
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", Rumbl do
   #   pipe_through :api
